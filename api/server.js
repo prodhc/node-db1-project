0bd27cc3b5a69d70321f707929a4356212ff12ba
prodhc <zionywood@gmail.com>
2021-06-07 18:02:35 -0700

    set up

--- api/server.js
+++ api/server.js
@@ -4,4 +4,10 @@ const server = express();
 
 server.use(express.json());
 
+server.use('*', (req, res) => {
+    res.status(400).json({
+        message: 'uh oh not found',
+    })
+})
+
 module.exports = server;
