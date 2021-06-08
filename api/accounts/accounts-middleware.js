1c1c7490d47a86cf8685b276491da2755e10739f
prodhc <zionywood@gmail.com>
2021-06-07 19:33:09 -0700

    post endpoint setup

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,4 +1,5 @@
 const Account = require('./accounts-model')
+const db = require('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
   const err = { status: 400 }
@@ -22,10 +23,20 @@ exports.checkAccountPayload = (req, res, next) => {
   }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('checkAccountNameUnique middleware')
-  next()
+exports.checkAccountNameUnique = async (req, res, next) => {
+ try {
+  const existing = await db('accounts')
+  .where('name', req.body.name.trim())
+  .first()
+
+  if (existing) {
+    next({ status: 400, message: 'that name is taken' })
+  } else {
+    next()
+  }
+ } catch (err) {
+    next(err)
+ }
 }
 
 exports.checkAccountId = async (req, res, next) => {
