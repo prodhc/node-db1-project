028dc76b3335fbc7951a99e0836b6cee4653e92a
prodhc <zionywood@gmail.com>
2021-06-07 18:38:00 -0700

    basic accounts router set up

--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
+const accountsRouter = require('./accounts/accounts-router')
+
 const server = express();
 
 server.use(express.json());
 
+server.use('/api/accounts', accountsRouter)
+
 server.use('*', (req, res) => {
-    res.status(400).json({
+    res.status(404).json({
         message: 'uh oh not found',
     })
 })
