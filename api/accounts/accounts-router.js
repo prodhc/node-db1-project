8a968680697ae89e416eb000a1574972fe673224
prodhc <zionywood@gmail.com>
2021-06-07 19:48:56 -0700

    put endpoint set up

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -29,8 +29,9 @@ try {
 }
 })
 
-router.put('/:id', md.checkAccountId, md.checkAccountPayload, md.checkAccountNameUnique, (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', md.checkAccountId, md.checkAccountPayload, async (req, res, next) => {
+  const updated = await Account.updateById(req.params.id, req.body)
+  res.json(updated)
 try {
   res.json('update account by id')
 } catch (err) {
